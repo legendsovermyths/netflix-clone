21a4f25f970753cac9ddb65c92194a50130a52d2
Mask Wolf <maskwolf2000@gmail.com>
2020-08-01 17:38:48 +0530

    request complete

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
       <h1>I am NETFLIX, believe me!</h1>
       <Row title="NETFLIX ORIGINALS" fetchURL={requests.fetchNetflixOriginals}/>
       <Row title="Trending Now" fetchURL={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchURL={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies}/>
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries}/>
 
     </div>
   );
--- src/Row.js
+++ src/Row.js
@@ -19,7 +19,7 @@ function Row({title,fetchURL}){
       <h2>{title}</h2>
       <div className="row_posters">
           {movies.map(movie=>(
-            <img className="row_poster" src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+            <img key={movie.id} className="row_poster" src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
           ))}
       </div>
     </div>
