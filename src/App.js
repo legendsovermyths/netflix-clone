b233a57f4e8ec9fec992c1412e464e7540dfce4e
Mask Wolf <maskwolf2000@gmail.com>
2020-08-01 18:02:01 +0530

    custom style for originals

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ function App() {
   return (
     <div className="App">
       <h1>I am NETFLIX, believe me!</h1>
-      <Row title="NETFLIX ORIGINALS" fetchURL={requests.fetchNetflixOriginals}/>
-      <Row title="Trending Now" fetchURL={requests.fetchTrending}/>
-      <Row title="Top Rated" fetchURL={requests.fetchTopRated}/>
-      <Row title="Action Movies" fetchURL={requests.fetchActionMovies}/>
-      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies}/>
-      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies}/>
-      <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies}/>
-      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries}/>
+      <Row isLargeRow={true} title="NETFLIX ORIGINALS" fetchURL={requests.fetchNetflixOriginals}/>
+      <Row isLargeRow={false} title="Trending Now" fetchURL={requests.fetchTrending}/>
+      <Row isLargeRow={false} title="Top Rated" fetchURL={requests.fetchTopRated}/>
+      <Row isLargeRow={false} title="Action Movies" fetchURL={requests.fetchActionMovies}/>
+      <Row isLargeRow={false} title="Comedy Movies" fetchURL={requests.fetchComedyMovies}/>
+      <Row isLargeRow={false} title="Horror Movies" fetchURL={requests.fetchHorrorMovies}/>
+      <Row isLargeRow={false} title="Romance Movies" fetchURL={requests.fetchRomanceMovies}/>
+      <Row isLargeRow={false} title="Documentaries" fetchURL={requests.fetchDocumentaries}/>
 
     </div>
   );
--- src/Row.js
+++ src/Row.js
@@ -2,7 +2,7 @@ import React,{useState, useEffect} from "react";
 import axios from './axios'
 import "./Row.css"
 const base_url="https://image.tmdb.org/t/p/original/"
-function Row({title,fetchURL}){
+function Row({title,fetchURL,isLargeRow}){
   const [movies,setMovies]=useState([]);
   useEffect(()=>{
       async function fetchData(){
@@ -19,7 +19,7 @@ function Row({title,fetchURL}){
       <h2>{title}</h2>
       <div className="row_posters">
           {movies.map(movie=>(
-            <img key={movie.id} className="row_poster" src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+            <img key={movie.id} className={`row_poster ${isLargeRow && `row_posterLarge`}`} src={`${base_url}${isLargeRow?movie.poster_path:movie.backdrop_path}`} alt={movie.name}/>
           ))}
       </div>
     </div>
