1df548dae6a7cda681f97803c308d5741b85f170
Mask Wolf <maskwolf2000@gmail.com>
2020-08-01 17:11:20 +0530

    add row posters

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import Row from './Row'
+import "./App.css"
+import requests from "./request"
 function App() {
   return (
     <div className="App">
-      <h1>I am Anirudh Singh</h1>
+      <h1>I am NETFLIX, believe me!</h1>
+      <Row title="NETFLIX ORIGINALS" fetchURL={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchURL={requests.fetchTrending}/>
+
     </div>
   );
 }
---
+++ src/Row.js
@@ -0,0 +1,28 @@
+import React,{useState, useEffect} from "react";
+import axios from './axios'
+import "./Row.css"
+const base_url="https://image.tmdb.org/t/p/original/"
+function Row({title,fetchURL}){
+  const [movies,setMovies]=useState([]);
+  useEffect(()=>{
+      async function fetchData(){
+        const request = await axios.get(fetchURL);
+        console.log(request);
+        setMovies(request.data.results)
+        return request;
+      }
+      fetchData();
+  },[fetchURL]);
+  console.log(movies);
+  return(
+    <div className='row'>
+      <h2>{title}</h2>
+      <div className="row_posters">
+          {movies.map(movie=>(
+            <img className="row_poster" src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+          ))}
+      </div>
+    </div>
+  )
+}
+export default Row
